96373a2be706f6269529f2b6a15cb65013e05f41
yimchanbin <ycbkr0719@dataworld.co.kr>
2022-09-09 23:26:35 +0900

    비밀번호 db 저장 시 암호화

--- README.md
+++ README.md
@@ -4,3 +4,4 @@
 # express npm install express --save          java의 스프링 같은 FrameWork
 # mongoose npm install mongoose --save        MongoDB를 편하게 사용할 수 있는 Tool
 # nodemone npm istall nodemon --save-dev      스크립트의 코드 변화가 있을 때 nodemon을 통해 실행을 했을 떄는 서버를 재기동 하지 않아도 반영할 수 있게 해줌 -dev는 로컬에서 할 떄랑 배포 이후에 할 떄의 모드가 있는데 -dev는 로컬에서 할 떄만 사용가능하게 하는 역할 package.json에 devDependencies에 추가 됨
+# bcrypt npm install bcrypt --save            클라이언트 단에서 비밀번호 입력후 DB에 저장 될 때 암호화 시켜줌
--- models/user.js
+++ models/user.js
@@ -4,6 +4,9 @@
 // 4. user모델 만들기
 
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+// salt를 이용해서 비밀번호를 암호화 시켜주는데 이 때 암호화 10글자로 암호화를 시켜줌
+const saltRounds = 10;
 
 const userSchema = mongoose.Schema({
     name :{
@@ -36,6 +39,37 @@ const userSchema = mongoose.Schema({
         type : Number
     }
 })
+// index에 register 라우터에서 user.save를 실행 하기 전에 해당 함수가 실행되고 함수가 종료되면 다음 로직이 실행됨
+userSchema.pre('save', function(next) {
+    // userSchema의 스키마들을 말함.
+    let user = this;
+    // 모델의 password가 변화할 때만 비밀번호 암호화를 진행한다.
+    // 해당 조건문이 없으면 비밀번호가 아닌 모든 정보에 대한 저장행위가 일어날 때 마다 비밀번호 암호화가 진행됨
+    if(user.isModified('password')){
+        // salt를 이용해서 비밀번호 암호화
+        // salt를 만들기 위해서는 saltRounds가 필요
+        bcrypt.genSalt(saltRounds, function(err, salt){
+            if(err){
+                // 에러가 발생했을 경우 err을 돌려보낸다
+                return next(err);
+                // 여기서 next는 index.js의 user.save 함수
+            }
+            // hash의 첫번째 인자로는 클라이언트에서 입력한 실제 비밀번호가 들어온다.
+            bcrypt.hash(user.password, salt, function(err, hash){
+                                                    // 위의 hash는 암호화된 비밀번호
+                if(err){
+                    // 에러가 발생했을 경우 next에 에러를 반환하고,
+                    return next(err)
+                }
+                // 클라이언트에서 입력한 비밀번호가 암호화에 성공 했다면
+                // 실제 입력한 비밀번호가 들어있던 스키마를 암호화된 비밀번호로 넣어준다.
+                user.password = hash;
+                next();
+            })
+        })
+    }
+});
+
 // model로 schema 감싸주기
 const User = mongoose.model('User', userSchema);
 
