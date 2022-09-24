174897b7b3c33465266672a557a9852aa800abad
travelSnail <ycbkr0719@gmail.com>
2022-09-24 22:45:38 +0900

    react-route-dom 설치 후 적용(6버전)

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,56 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
 
 
 // 메인화면이 실행됬을 App.js를 랜더링해서 보여주는 화면
 // 이 App.js 는 index.js에서 컴포넌트를 넣어줬기 때문에 시작화면에서 뜨게되는 것.
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    // react-router-dom은 6버전 이상부터는 Switch대신 Routes를 사용하고
+    // Routes의 자식요소로는 Route만 올 수 있다.
+    // 그리고 컴포넌트 대신 element를 사용한다.
+    <BrowserRouter>
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/users">Users</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <Routes>
+        <Route path="/about" element = {<About />} />
+        <Route path="/users" element = {<Users />} />
+        <Route path="/" element = {<Home />} />
+      </Routes>
     </div>
+  </BrowserRouter>
   );
 }
 
 export default App;
+
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function Users() {
+  return <h2>Users</h2>;
+}
