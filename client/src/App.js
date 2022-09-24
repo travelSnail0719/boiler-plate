852b68c71ed8e5b85edfb1e63574a1f6cb2d1d92
travelSnail <ycbkr0719@gmail.com>
2022-09-24 22:58:21 +0900

    6버전 문법으로 변경 후 컴포넌트 교체

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,9 @@ import {
   Route,
   Link
 } from "react-router-dom";
-
+import LandingPage from './components/views/LandingPage/LandingPage'
+import LoginPage from './components/views/LoginPage/LoginPage';
+import RegisterPage from './components/views/RegisterPage/RegisterPage'
 
 // 메인화면이 실행됬을 App.js를 랜더링해서 보여주는 화면
 // 이 App.js 는 index.js에서 컴포넌트를 넣어줬기 때문에 시작화면에서 뜨게되는 것.
@@ -14,43 +16,15 @@ function App() {
   return (
     // react-router-dom은 6버전 이상부터는 Switch대신 Routes를 사용하고
     // Routes의 자식요소로는 Route만 올 수 있다.
-    // 그리고 컴포넌트 대신 element를 사용한다.
+    // 그리고 component 대신 element를 사용한다.
     <BrowserRouter>
-    <div>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-          <li>
-            <Link to="/users">Users</Link>
-          </li>
-        </ul>
-      </nav>
-
       <Routes>
-        <Route path="/about" element = {<About />} />
-        <Route path="/users" element = {<Users />} />
-        <Route path="/" element = {<Home />} />
+        <Route path="/" element = {<LandingPage />} />
+        <Route path="/login" element = {<LoginPage />} />
+        <Route path="/register" element = {<RegisterPage />} />
       </Routes>
-    </div>
-  </BrowserRouter>
+    </BrowserRouter>
   );
 }
 
 export default App;
-
-function Home() {
-  return <h2>Home</h2>;
-}
-
-function About() {
-  return <h2>About</h2>;
-}
-
-function Users() {
-  return <h2>Users</h2>;
-}
